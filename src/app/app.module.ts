1ab9b8156d1fcdb4a42465291105addc5b7a9622
Sofia Medina <sofi.k.medina@gmail.com>
2024-01-12 00:07:24 -0300

    Revert "implement sass"
    This reverts commit 3640067fde762cb4822cccb196fc3de7893bee42.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
