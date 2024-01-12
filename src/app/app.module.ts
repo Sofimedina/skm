c7cb07db2b789503ee3e363a630e6722ddbb21cb
Sofia Medina <sofi.k.medina@gmail.com>
2024-01-12 01:36:58 -0300

    adding components
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './loader/loader.component';
-import { LoadingService } from './services/loading.service'; // Import LoadingService
+import { LoadingService } from './services/loading.service';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component'; // Import LoadingService
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoaderComponent
+    LoaderComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+// header.component.ts
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  
+}
