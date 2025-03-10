149bdf4ccab25cc1a78315f57060f67e1190916e
Sofia Medina <sofi.k.medina@gmail.com>
2025-03-10 02:19:48 -0300

    c
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/skm",
-  output: "export",
-  reactStrictMode: true,
+  output: "export", // This enables static HTML export
+  basePath: "/skm", // Replace with your repository name
+  assetPrefix: "/skm/",
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js Image Optimization
+  },
 };
-  
+
 module.exports = nextConfig;
